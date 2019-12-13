e6a71988a8cda61ae21c3e79db9e619ebc26ebc6
berihacher <berihacher@outlook.com>
2019-12-13 19:14:07 +0800

    Updates

---
+++ precache-manifest.fab17bb1250f473232ecc97ea4c50d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817a8e38c2392bc0f350d5a60ab597b6",
+    "url": "/Complete-React-Developer-1/index.html"
+  },
+  {
+    "revision": "65440588a2828e9db32c",
+    "url": "/Complete-React-Developer-1/static/css/main.6a2bb71f.chunk.css"
+  },
+  {
+    "revision": "0b945ebf1174527ff9a8",
+    "url": "/Complete-React-Developer-1/static/js/2.c9f143e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Complete-React-Developer-1/static/js/2.c9f143e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65440588a2828e9db32c",
+    "url": "/Complete-React-Developer-1/static/js/main.7049f0ac.chunk.js"
+  },
+  {
+    "revision": "4c817c3e629e29fdb808",
+    "url": "/Complete-React-Developer-1/static/js/runtime-main.78058910.js"
+  }
+]);
